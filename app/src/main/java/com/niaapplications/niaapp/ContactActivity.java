b001043033ba6c4f17be538711d9f92b42699f7a
Nia Williams <niaapplications@gmail.com>
2021-10-06 20:55:29 -0400

    contact update

--- app/src/main/java/com/niaapplications/niaapp/ContactActivity.java
+++ app/src/main/java/com/niaapplications/niaapp/ContactActivity.java
@@ -12,5 +12,7 @@ public class ContactActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         EditText name = findViewById(R.id.name_box);
+        EditText email = findViewById(R.id.email_box);
+        EditText msg = findViewById(R.id.msg);
     }
 }
